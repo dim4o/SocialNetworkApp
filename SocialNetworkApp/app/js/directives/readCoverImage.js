e4b05785df1d6c64d52ebdb494e25ae1a4a3c770
dim4o <d_i_m@abv.bg>
2015-05-30 22:23:10 +0300

    Added edit profile validation with notifications for fields and images.

--- SocialNetworkApp/app/js/directives/readCoverImage.js
+++ SocialNetworkApp/app/js/directives/readCoverImage.js
@@ -1,4 +1,4 @@
-socialNetworkApp.directive("readCoverImage", [function () {
+socialNetworkApp.directive("readCoverImage", ['notificationService', function (notificationService) {
     return {
         scope: {
             readCoverImage: "="
@@ -12,9 +12,20 @@ socialNetworkApp.directive("readCoverImage", [function () {
                     });
                 };
 
-                if (changeEvent.target.files[0] &&
-                    changeEvent.target.files[0].size > 1024000) {
-                    console.log('Error!');
+                file = changeEvent.target.files[0];
+                if (file && !file.type.match(/image\/.*/)) {
+
+                    attributes.$observe('readCoverImage', function () {
+                        notificationService.error('Invalid file format.',
+                            'Allowed format: .jpg ');
+                    })
+                } else if(file && file.size > 1024000){
+
+
+                    attributes.$observe('readCoverImage', function () {
+                        notificationService.error('File size limit exceeded.',
+                            'The maximum upload size for profile image is 1024kb.');
+                    })
                 } else {
                     reader.readAsDataURL(changeEvent.target.files[0]);
                 }
--- SocialNetworkApp/app/js/directives/readProfileImage.js
+++ SocialNetworkApp/app/js/directives/readProfileImage.js
@@ -5,25 +5,30 @@ socialNetworkApp.directive("readProfileImage", ['notificationService', function 
         },
         link: function (scope, element, attributes) {
             element.bind("change", function (changeEvent) {
-                var reader = new FileReader();
+                var reader = new FileReader(),
+                    file;
+
                 reader.onload = function (loadEvent) {
                     scope.$apply(function () {
                         scope.readProfileImage = loadEvent.target.result;
                     });
                 };
-                if (changeEvent.target.files[0] &&
-                    changeEvent.target.files[0].size > 128000) {
-                    console.log('Error!');
-                    console.log(changeEvent.target.files[0].name);
-                    notificationService.error('Error', 'Aasdfsdfd');
-                    attributes.$observe('readProfileImage', function (value) {
-                        console.log(value);
+                file = changeEvent.target.files[0];
+                if (file && !file.type.match(/image\/.*/)) {
+
+                    attributes.$observe('readProfileImage', function () {
+                        notificationService.error('Invalid file format.',
+                            'Allowed format: .jpg ');
+                    })
+                } else if(file && file.size > 128000){
+
+
+                    attributes.$observe('readProfileImage', function () {
+                        notificationService.error('File size limit exceeded.',
+                            'The maximum upload size for profile image is 128kb.');
                     })
                 } else {
                     reader.readAsDataURL(changeEvent.target.files[0]);
-                    attributes.$observe('readProfileImage', function (value) {
-                        console.log(value);
-                    })
                 }
             });
         }
