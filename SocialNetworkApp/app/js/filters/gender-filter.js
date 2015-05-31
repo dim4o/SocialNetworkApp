75b72c9fa6af397ecd3c9525607dcf56bd8850f4
dim4o <dimcho.nedev+git@gmail.com>
2015-05-31 11:40:10 +0300

    Delete gender-filter.js

--- SocialNetworkApp/app/js/filters/gender-filter.js
+++
@@ -1,9 +0,0 @@
-socialNetworkApp.filter('gender', function gender() {
-    return function (gender) {
-        switch(gender) {
-            case 0: return 'Other';
-            case 1: return 'Male';
-            case 2: return 'Female';
-        }
-    }
-});
